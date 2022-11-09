eb54073ce79b4b2fbf04017bb677cd9be7ec74c0
Rubén Lastres Villar <rlastresvillar@gmail.com>
2022-11-09 19:08:47 +0100

    make it better

--- src/main/java/com/rlastres/faunagalicia/FaunagaliciaApp.java
+++ src/main/java/com/rlastres/faunagalicia/FaunagaliciaApp.java
@@ -2,8 +2,10 @@ package com.rlastres.faunagalicia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.rlastres.faunagalicia")
 public class FaunagaliciaApp {
 
 	public static void main(String[] args) {
